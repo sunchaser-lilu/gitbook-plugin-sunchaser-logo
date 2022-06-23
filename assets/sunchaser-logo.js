c8c0819200a8b9d4965cb3aca2aa8ecabdd21763
lilu <admin@lilu.org.cn>
2022-06-23 16:36:53 +0800

    update version 1.0.1

--- assets/sunchaser-logo.js
+++ assets/sunchaser-logo.js
@@ -1,18 +1,20 @@
 require(['gitbook', 'jQuery'], function (gitbook, $) {
-  var url = ''
-  var style = ''
+  var url = '';
+  var style = '';
+  var redirectUrl = '';
   var logo = function (url, style) {
     $('.book-summary').children()
       .eq(0)
       .before($('<div class="book-logo"><img src="' + url + '" style="' + style + '" /></div>')
         .click(function () {
-          window.location.href = "/";
+          window.location.href = redirectUrl;
         })
       )
   }
   gitbook.events.bind('start', function (e, config) {
-    url = config['sunchaser-logo']['url']
-    style = config['sunchaser-logo']['style']
+    url = config['sunchaser-logo']['url'];
+    style = config['sunchaser-logo']['style'];
+    redirectUrl = config['sunchaser-logo']['redirectUrl'];
   })
 
   gitbook.events.bind("page.change", function () {
